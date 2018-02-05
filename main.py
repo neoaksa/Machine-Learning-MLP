d642f54ad38600f786b4d3f9ab2ba9776d8112a6
Jie Tao <neo_aksa@hotmail.com>
2018-02-05 10:44:12 -0500

    add cutwhitespace function

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ import seaborn as sn
 import pandas as pd
 
 def main():
-    whitecells()
-    #training()
-    #testing()
+     # whitecells()
+     # training()
+     testing()
 
 def testing():
     # load data from csv
@@ -22,7 +22,7 @@ def testing():
     testing_label = genfromtxt(currentpath + '/data/test_labels.csv',
                                delimiter=',',
                                skip_header=1)
-    testing_input = normalizationX(testing_file)  # normalization data
+    testing_input = normalizationX(cutwhitespace(testing_file))  # normalization data
     testing_input = np.transpose(testing_input)  # transpose
     testing_input = np.split(testing_input, len(testing_input[0]), axis=1)
     testing_tag = [YtoOutput(y) for y in testing_label]  # construct tag for output node
@@ -38,15 +38,17 @@ def testing():
     amlp = mlp.mlp(None, None, None, None, 1, 0)
     np.savetxt('test_result.csv', amlp.perdict(testing_input), delimiter=',')
     evaluate = amlp.evaluate(testing_data)
-    print(amlp.perdict(testing_input))
-    print(testing_label)
-    type(testing_input)
+    # print(amlp.perdict(testing_input))
+    # print(testing_label)
+    # type(testing_input)
     print("Correct in Test: {0} % ".format(evaluate/len(testing_label)*100))
+    # plot a confusion_matrix for testing result
     pred = amlp.perdict(testing_input)
     cmat = confusion_matrix(testing_label, pred)
     print(cmat)
     df_cm = pd.DataFrame(cmat, range(10), range(10))
     sn.set(font_scale=1.4)
+    # feed confusion_matrix to heatmap
     sn.heatmap(df_cm,annot=True,annot_kws={"size": 12},fmt='g',cmap='Blues',vmax=25)
     plt.show()
 
@@ -59,12 +61,12 @@ def training():
                                skip_header=1,
                                max_rows=4000)
     # split training data into training and validation, 50 : 25
-    training_input = normalizationX(training_file[0::2, 1:])  # normalization data
+    training_input = normalizationX(cutwhitespace(training_file[0::2, 1:]))  # normalization data
     training_input = np.transpose(training_input)  # transpose
     training_input = np.split(training_input, len(training_input[0]), axis=1)
     training_tag = [YtoOutput(y) for y in training_file[0::2, 0]]  # construct tag for output node
     training_data = list(zip(training_input, training_tag))
-    validation_input = normalizationX(training_file[1::4, 1:])  # normalization data
+    validation_input = normalizationX(cutwhitespace(training_file[1::4, 1:]))  # normalization data
     validation_input = np.transpose(validation_input)  # transpose
     validation_input = np.split(validation_input, len(validation_input[0]), axis=1)
     validation_tag = [YtoOutput(y) for y in training_file[1::4, 0]]  # construct tag for output node
@@ -76,14 +78,14 @@ def training():
 
     # construct mlp object
     # lr=learning rate=0.3,
-    # momentum=0.5,                     
+    # momentum=0.5,
     # size=None,
     # epoch=100,
     # load=load weights from npz file=0
     # save=save new module structure into npy files
-    # 784 inputs, 1 hidden layer with 50 nueros, 10 output.
+    # 784 inputs(may cut witespace), 1 hidden layer with 50 nueros, 10 output.
     # in this case, we can only modify hidden layer of mlp structure
-    size = [784, 50, 10]
+    size = [len(training_input[0]), 50, 10]
     amlp = mlp.mlp(0.9, 0.3, size, 200, 0, 1)
     amlp.miniBatch(training_data, 50, validation_date)
 
@@ -97,6 +99,7 @@ def YtoOutput(y):
 def normalizationX(x):
     return x / 255
 
+# visualize the white space according to the mean of each cells in all samples
 def whitecells():
     # load data from csv
     currentpath = os.getcwd()
@@ -104,10 +107,17 @@ def whitecells():
                                delimiter=',',
                                skip_header=1,
                                max_rows=40000)
-    meanarray = np.mean(training_file1[:,1:],axis=0)
+    meanarray = np.mean(training_file[:,1:],axis=0)
     meanarray = meanarray.reshape(28,28)
     plt.imshow(meanarray, cmap='hot', interpolation='nearest')
     plt.show()
+
+# according to whitecells(), cut whitespace
+def cutwhitespace(input):
+    ref = np.arange(0, 784).reshape(28, 28)
+    ref = ref[4:23, 4:23]
+    input = input[:,ref.flatten()]
+    return input
 
 if __name__ == '__main__':
     start_time = time.time()
--- test.py
+++ test.py
@@ -2,29 +2,46 @@ from numpy import genfromtxt
 import os
 import numpy as np
 
+
+def normalizationX(x):
+    return x / 255
+
 currentpath = os.getcwd()
-training_data = genfromtxt(currentpath+'/data/train.csv',delimiter=',',skip_header=1, max_rows=100)
-# split traning data into two parts: inputs and tag test_data
-# print(training_data[:,1:])
-# # print(training_data[:,0])
-# x3 = zip(training_data[:,1:],training_data[:,0])
-# x3 = list(x3)
-# np.random.shuffle(x3)
-# mini_batches = [
-#     x3[k:k+10]
-#     for k in range(0,len(x3),10)
-# ]
-# i=0
-# for x,y in mini_batches[0]:
-#     print(i)
-#     print(x)
-#     print(y)
-#     i+=i
-# mini_batches = [
-#     x3[k:k+3]
-#     for k in range(0,len(list(x3)),3)
-# ]
-# print(mini_batches)
-a = np.random.randn(10,1)
-print(np.argmax(a))
-print(a)
+training_file = genfromtxt(currentpath+'/data/train.csv',delimiter=',',skip_header=1, max_rows=100)
+training_input = normalizationX(training_file[0::2, 1:])  # normalization data
+# # split traning data into two parts: inputs and tag test_data
+# # print(training_data[:,1:])
+# # # print(training_data[:,0])
+# # x3 = zip(training_data[:,1:],training_data[:,0])
+# # x3 = list(x3)
+# # np.random.shuffle(x3)
+# # mini_batches = [
+# #     x3[k:k+10]
+# #     for k in range(0,len(x3),10)
+# # ]
+# # i=0
+# # for x,y in mini_batches[0]:
+# #     print(i)
+# #     print(x)
+# #     print(y)
+# #     i+=i
+# # mini_batches = [
+# #     x3[k:k+3]
+# #     for k in range(0,len(list(x3)),3)
+# # ]
+# # print(mini_batches)
+# a = np.random.randn(10,1)
+# print(np.argmax(a))
+# print(a)
+
+t = np.arange(0,784).reshape(28,28)
+#print(t)
+t=t[4:23, 4:23 ]
+print(t[1])
+#print(t.flatten())
+n = training_input[:, t.flatten()]
+print(n[1])
+
+
+
+
